0a388f0f2c119ca8855ef1dfbcf67d68f4eb3898
Harendra Kumar Kanojiya <harendraverma21@gmail.com>
2024-05-18 18:13:18 +0530

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ export const environment = {
       logo: "https://cloud.appwrite.io/v1/storage/buckets/658da457358d3cfe6311/files/65c9edd7433dda89da12/preview?width=500&height=0&gravity=center&quality=90&project=64f1a389936d60327f76",
       keywords: "python, code"  
     },
-    v1Api: "https://with-code-example.web.app/api/v1",
-    apiHost: "https://with-code-example.web.app",
+    v1Api: "https://social-app-dtfau3loxa-uc.a.run.app/api/v1",
+    apiHost: "https://social-app-dtfau3loxa-uc.a.run.app",
     authSuccess: "https://content-clock.web.app/auth/success",
     authFailure: "https://content-clock.web.app/auth/failure"
 }
