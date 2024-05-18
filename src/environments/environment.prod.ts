bbdd62f1c61ac2427a0dc85cf1c66a486c1d4331
Harendra Kumar Kanojiya <harendraverma21@gmail.com>
2024-05-18 17:29:53 +0530

    env changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    config: {
+      title: "Content Clock",
+      description: "Content Clock",
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/658da457358d3cfe6311/files/65c9edd7433dda89da12/preview?width=500&height=0&gravity=center&quality=90&project=64f1a389936d60327f76",
+      keywords: "python, code"  
+    },
+    v1Api: "https://with-code-example.web.app/api/v1",
+    apiHost: "https://with-code-example.web.app",
+    authSuccess: "https://content-clock.web.app/auth/success",
+    authFailure: "https://content-clock.web.app/auth/failure"
+}
+// https://social-app-dtfau3loxa-uc.a.run.app
